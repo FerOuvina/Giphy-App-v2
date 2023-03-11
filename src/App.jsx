c416c3c52baba18a92893557a2d8f9a2d7550286
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-03-11 19:16:43 -0300

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import { Link, Route } from "wouter";
+import { Route } from "wouter";
 import Home from "./pages/Home";
 import Logo from "./components/Logo";
 import Gifs from "./pages/Gifs";
-import Search from './components/Search'
+import Search from "./components/Search";
+import GifDetail from "./components/GifDetail";
+import { GifsContextProvider } from "./context/gifContext";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <section className="header">
-        <Logo />
-        <Search />
-        <Route component={Home} path="/Giphy-App-v2"></Route>
-      </section>
-
-      <section className="gifsContainer">
-        <Route component={Gifs} path="/Giphy-App-v2/search/:keyword" />
-      </section>
-    </div>
+    <GifsContextProvider>
+      <div className="App">
+        <section className="header">
+          <Logo />
+          <Search />
+          <Route component={Home} path="/Giphy-App-v2"></Route>
+        </section>
+        <section className="gifsContainer">
+          <Route component={Gifs} path="/Giphy-App-v2/search/:keyword" />
+          <Route component={GifDetail} path="/Giphy-App-v2/gif/:id" />
+        </section>
+      </div>
+    </GifsContextProvider>
   );
 }
 
