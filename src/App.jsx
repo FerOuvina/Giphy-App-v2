2a7bfbaf6934941518a9ec72df9cfe2506e5d6f0
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-03 19:02:23 -0300

    Modifying the main render components

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Gifs from "./pages/Gifs";
 import Search from "./components/Search";
 import GifDetail from "./components/GifDetail";
 import { GifsContextProvider } from "./context/gifContext";
-import TrendingSearches from "./components/TrendingSearches";
+import LazyTrending from "./components/TrendingLazy";
 import "./App.css";
 
 function App() {
@@ -17,12 +17,11 @@ function App() {
           <Logo />
           <Search />
           <Route component={Home} path="/Giphy-App-v2" />
-          <Route component={TrendingSearches} path="/Giphy-App-v2" />
         </section>
         <section className="gifsContainer">
           <Route component={Gifs} path="/Giphy-App-v2/search/:keyword" />
           <Route component={GifDetail} path="/Giphy-App-v2/gif/:id" />
-          <Route component={TrendingSearches} path="/Giphy-App-v2/gif/:id" />
+          <Route component={LazyTrending} path="/Giphy-App-v2/gif/:id" />
         </section>
       </div>
     </GifsContextProvider>
