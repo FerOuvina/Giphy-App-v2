b527b5a1f9878c94a619206ef0b47acf1183b0e7
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-06 14:08:27 -0300

    Removing button for pagination and implementing Infinite Scroll

--- src/components/ListOfGifs.jsx
+++ src/components/ListOfGifs.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef, useCallback } from "react";
 import Gif from "./Gif";
 import useGifs from "../hooks/useGifs";
 import "../stylesheets/ListOfGifs.css";
+import useObserver from "../hooks/useObserver";
+import debounce from "just-debounce-it";
 
 export default function ListOfGifs({ params }) {
   const { keyword } = params;
   const { gifs, setPage } = useGifs({ keyword });
+  const externalRef = useRef();
+  const { isNearScreen } = useObserver({ distance : '200px', externalRef, once: false });
+  console.log(isNearScreen)
 
-  const handlePage = () => setPage((prevPage) => prevPage + 1);
+  const debounceHandleNextPage = useCallback(debounce(() => {
+    setPage((prevPage) => prevPage + 1), 200
+  }), [])
+
+  useEffect(function(){
+    if (isNearScreen) debounceHandleNextPage();
+  }, [debounceHandleNextPage, isNearScreen])
 
   return (
     <div>
@@ -16,9 +27,7 @@ export default function ListOfGifs({ params }) {
           <Gif title={title} url={url} key={id} id={id} />
         ))}
       </div>
-      <div className="btnContainer">
-        <button onClick={handlePage}>Show More</button>
-      </div>
+      <div className="watch" ref={externalRef}></div>
     </div>
   );
 }
