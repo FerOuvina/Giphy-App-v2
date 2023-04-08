be9dd6ccaf8dfd3abad2f30f4e2c70bf859e924b
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-08 20:00:26 -0300

    Fixing stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Route } from "wouter";
 import Home from "./pages/Home";
 import Logo from "./components/Logo";
