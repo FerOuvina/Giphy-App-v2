3af94e1677407b1a54d1d7b336fadecef4193646
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-07 21:17:39 -0300

    Setting more settings

--- src/services/setting.jsx
+++ src/services/setting.jsx
@@ -1,3 +1,4 @@
+// Required for getGifs and getTrendingGifs
 export const API_KEY = "hFv8OeXUU3hh5g3av7nTj9gMJZxGgcmL";
 export const API_URL = "https://api.giphy.com/v1";
 export const GIF__AMOUNT = 11;
