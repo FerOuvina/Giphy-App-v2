cb89fe2f2dc4fb9409e089c864c099db295a1d5a
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-09 20:54:40 -0300

    New hook for using gif by id

---
+++ src/hooks/useGifById.jsx
@@ -0,0 +1,21 @@
+import getGifById from "../services/getGifById";
+import { useEffect, useState } from "react";
+import { useGifs } from "./useGifs";
+
+export default function useGifById({ id }) {
+  const { gifs } = useGifs();
+  const gifFromCache = gifs.find((singleGif) => singleGif.id === id);
+
+  const [gif, setGif] = useState(gifFromCache);
+
+  useEffect(
+    function () {
+      if (!gif) {
+        // Call service if we don't have a gif
+        getGifById({ id }).then((gif) => setGif(gif));
+      }
+    },
+    [gif, id]
+  );
+  return { gif };
+}
