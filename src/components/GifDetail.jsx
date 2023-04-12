cb58b2abc32c15b09e0dccc8bfe4eecabdebaa83
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-12 14:57:43 -0300

    Adding conditional rendering for error and loading states

--- src/components/GifDetail.jsx
+++ src/components/GifDetail.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Redirect } from "wouter";
 import useGifById from "../hooks/useGifById";
 import Gif from "./Gif";
+import Spinner from '../components/LoadingSpinner'
 
 export default function ({ params }) {
-  const { gif } = useGifById({ id: params.id });
+  const { gif, Loading, Error } = useGifById({ id: params.id });
+
+  if (Loading) return <Spinner />;
+  if (Error) return <Redirect to={'404'} />;
+  if (!gif) return null;
 
   return <Gif {...gif} />;
 }
