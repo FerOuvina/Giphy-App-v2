1a760249d873d1df0e922078d2965c52652b6788
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-12 14:56:48 -0300

    Creating and styling LoadingSpinner component

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../stylesheets/LoadingSpinner.css'
+
+export default function Spinner () {
+  return (
+  <div class="loadingio-spinner-ellipsis-120pq448a5zq"><div class="ldio-9k0lcfnjnsr">
+<div></div><div></div><div></div><div></div><div></div>
+</div></div>
+  )
+
+}
