3429ef9f2192fdb1684f5574f0cfefb473080927
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-14 17:55:37 -0300

    Adding a condition for searching and clearing user input after search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,9 +7,16 @@ export default function Search() {
   const [keyword, setKeyword] = useState("");
   const [path, setLocation] = useLocation();
 
+  const input = document.querySelector(".input");
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    setLocation(`/Giphy-App-v2/search/${keyword}`);
+    if (keyword) {
+      setLocation(`/Giphy-App-v2/search/${keyword}`);
+      input.value = "";
+    } else {
+      return;
+    }
   };
   const handleChange = (event) => {
     setKeyword(event.target.value);
@@ -22,6 +29,7 @@ export default function Search() {
           onChange={handleChange}
           type="text"
           placeholder="Search gifs here..."
+          className="input"
         />
         <div className="formContainer__form--btnContainer">
           <button type="submit">
