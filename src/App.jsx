976587e6fb0e2d40d0742b852bbf99c01a5717e9
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-14 17:32:23 -0300

    Adding app wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,20 @@ import "./App.css";
 function App() {
   return (
     <GifsContextProvider>
-      <div className="App">
-        <section className="header">
-          <Logo />
-          <Search />
-          <Route component={Home} path="/Giphy-App-v2" />
-        </section>
-        <section className="gifsContainer">
-          <Route component={Error} path="/Giphy-App-v2/gif/404" />
-          <Route component={Gifs} path="/Giphy-App-v2/search/:keyword" />
-          <Route component={GifDetail} path="/Giphy-App-v2/gif/:id" />
-        </section>
-      </div>
+      <main className="App">
+        <div className="appWrapper">
+          <section className="header">
+            <Logo />
+            <Search />
+            <Route component={Home} path="/Giphy-App-v2" />
+          </section>
+          <section className="gifsContainer">
+            <Route component={Error} path="/Giphy-App-v2/gif/404" />
+            <Route component={Gifs} path="/Giphy-App-v2/search/:keyword" />
+            <Route component={GifDetail} path="/Giphy-App-v2/gif/:id" />
+          </section>
+        </div>
+      </main>
     </GifsContextProvider>
   );
 }
