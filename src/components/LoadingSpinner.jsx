c14c4b97b0d1cfd875ead9ed4bbebf964023c8a6
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-14 14:41:48 -0300

    Fixing styles for the spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,9 +3,11 @@ import '../stylesheets/LoadingSpinner.css'
 
 export default function Spinner () {
   return (
+    <div className="container">
   <div className="loadingio-spinner-ellipsis-120pq448a5zq"><div className="ldio-9k0lcfnjnsr">
 <div></div><div></div><div></div><div></div><div></div>
 </div></div>
+    </div>
   )
 
 }
