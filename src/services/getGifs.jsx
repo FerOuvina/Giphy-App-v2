ff932d3cfbfbe1c5a9b583c2bd8c968c6c46633f
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-15 12:35:57 -0300

    Updating service to work with rating

--- src/services/getGifs.jsx
+++ src/services/getGifs.jsx
@@ -1,6 +1,6 @@
 import { API_KEY, API_URL, GIF__AMOUNT } from "./settings";
-export default async function getGifs({ keyword, page = 0 } = {}) {
-  const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${GIF__AMOUNT}&offset=${page * GIF__AMOUNT}&rating=g&lang=en`;
+export default async function getGifs({ keyword, page = 0, rating } = {}) {
+  const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${GIF__AMOUNT}&offset=${page * GIF__AMOUNT}&rating=${rating}&lang=en`;
 
   const res = await fetch(apiURL);
   const response = await res.json();
