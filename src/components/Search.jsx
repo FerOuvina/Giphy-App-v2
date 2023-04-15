ee59f8344643be8706e7047bb46c2ade5bf43fa6
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-15 12:36:33 -0300

    Implementing search with rating using a hook and React reducer

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,30 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
 import useLocation from "wouter/use-location";
 import { AiOutlineFileSearch } from "react-icons/ai";
+import { RATINGS } from "../services/settings";
+import useForm from "../hooks/useForm";
 import "../stylesheets/Search.css";
 
-export default function Search() {
-  const [keyword, setKeyword] = useState("");
+export default function Search({
+  initialRating = RATINGS[0],
+  initialKeyword = "",
+}) {
   const [path, setLocation] = useLocation();
+
+  const { keyword, rating, updateKeyword, updateRating } = useForm({
+    initialKeyword,
+    initialRating,
+  });
 
   const input = document.querySelector(".input");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (keyword) {
-      setLocation(`/Giphy-App-v2/search/${keyword}`);
+      setLocation(`/Giphy-App-v2/search/${keyword}/${rating}`);
       input.value = "";
     } else {
       return;
     }
   };
+
   const handleChange = (event) => {
-    setKeyword(event.target.value);
+    updateKeyword(event.target.value);
+  };
+
+  const handleRatingChange = (event) => {
+    updateRating(event.target.value);
   };
 
   return (
     <div className="formContainer">
       <form onSubmit={handleSubmit} className="formContainer__form">
+        <select onChange={handleRatingChange} value={rating}>
+          <option disabled>Select Rating</option>
+          {RATINGS.map((rating) => (
+            <option key={rating}>{rating}</option>
+          ))}
+        </select>
         <input
           onChange={handleChange}
           type="text"
