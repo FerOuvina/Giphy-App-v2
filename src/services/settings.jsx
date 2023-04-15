b5b12dcb8eeb97a68a693526d41660f955f95d6e
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-15 12:35:36 -0300

    Updating settings for new hook

--- src/services/settings.jsx
+++ src/services/settings.jsx
@@ -1,4 +1,11 @@
-// Required for services
+// Options for services
 export const API_KEY = "hFv8OeXUU3hh5g3av7nTj9gMJZxGgcmL";
 export const API_URL = "https://api.giphy.com/v1";
 export const GIF__AMOUNT = 11;
+
+// Options for Search Form hook
+export const RATINGS = ["g", "pg", "pg-13", "r"];
+export const ACTIONS = {
+  SET_KEYWORD: "SET_KEYWORD",
+  SET_RATING: "SET_RATING",
+};
