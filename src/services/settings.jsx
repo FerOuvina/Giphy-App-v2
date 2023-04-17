57782130759d27aba843d53c538a11c6811f4cb9
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-17 11:30:34 -0300

    Increasing the minimum gif amount to render

--- src/services/settings.jsx
+++ src/services/settings.jsx
@@ -1,7 +1,7 @@
 // Options for services
 export const API_KEY = "hFv8OeXUU3hh5g3av7nTj9gMJZxGgcmL";
 export const API_URL = "https://api.giphy.com/v1";
-export const GIF__AMOUNT = 11;
+export const GIF__AMOUNT = 21;
 
 // Options for Search Form hook
 export const RATINGS = ["g", "pg", "pg-13", "r"];
