6da2b6cf64a1bd9a12d9d1a5133340bbf29738ce
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-04-17 10:20:04 -0300

    Finishing the error 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,40 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "wouter";
+import '../stylesheets/404.css'
+
+const gifsID = [
+  "14uQ3cOFteDaU",
+  "9J7tdYltWyXIY",
+  "UoeaPqYrimha6rdTFV",
+];
 
 export default function Error() {
-  return <h1>Error 404 &gt;:(</h1>;
+  const randomGif = () => {
+    return `https://media.giphy.com/media/${
+      gifsID[Math.floor(Math.random() * gifsID.length) + 1]
+    }/giphy.gif`;
+  };
+
+  return (
+    <div className="errorContainer">
+      <Helmet>
+        <title>Error 404 | Giffy</title>
+      </Helmet>
+
+      <div className="errorContainer__gif">
+        <img
+          src={randomGif()}
+          alt="error 404 gif not found"
+          className="errorContainer__gif--img"
+        />
+      </div>
+
+      <div className="errorContainer__btn">
+        <Link to="/Giphy-App-v2">
+          <button>Go back home</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
